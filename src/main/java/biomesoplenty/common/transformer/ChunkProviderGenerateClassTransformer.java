b5dae0042d0c81195ad30aa0a0f2937f27b36027
andreblanke <blanke.and@gmail.com>
2020-05-30 02:36:54 +0200

    Fix every WorldType mistakenly getting the properties of WorldType.AMPLIFIED

--- src/main/java/biomesoplenty/common/transformer/ChunkProviderGenerateClassTransformer.java
+++ src/main/java/biomesoplenty/common/transformer/ChunkProviderGenerateClassTransformer.java
@@ -1,5 +1,8 @@
 package biomesoplenty.common.transformer;
 
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.world.WorldType;
 
@@ -13,7 +16,6 @@ import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.JumpInsnNode;
-import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -21,6 +23,7 @@ import biomesoplenty.common.core.BOPBiomes;
 
 import static org.objectweb.asm.Opcodes.*;
 
+/** {@link net.minecraft.world.gen.ChunkProviderGenerate} */
 public final class ChunkProviderGenerateClassTransformer implements IClassTransformer
 {
     private static final String TARGET_CLASS = "net.minecraft.world.gen.ChunkProviderGenerate";
@@ -37,10 +40,10 @@ public final class ChunkProviderGenerateClassTransformer implements IClassTransf
 
         LogManager.getLogger().info("Transforming {}", TARGET_CLASS);
 
-        final String targetMethodName = BiomesOPlentyCore.isObfuscated() ? "a"  : "func_147423_a";
         for (MethodNode method : node.methods)
         {
-            if (method.name.equals(targetMethodName) && method.desc.equals("(III)V"))
+            if (method.name.equals(BiomesOPlentyCore.isObfuscated() ? "a"  : "func_147423_a")
+                    && method.desc.equals("(III)V"))
             {
                 transformFunc_147423_a(method);
                 break;
@@ -53,46 +56,57 @@ public final class ChunkProviderGenerateClassTransformer implements IClassTransf
     }
 
     @SuppressWarnings("SpellCheckingInspection")
-    private void transformFunc_147423_a(final MethodNode method)
-    {
+    private void transformFunc_147423_a(final MethodNode method) {
         final boolean obfuscated = BiomesOPlentyCore.isObfuscated();
 
-        for (AbstractInsnNode abstractInsn : method.instructions.toArray())
-        {
-            if (abstractInsn.getOpcode() != GETSTATIC)
-                continue;
-            final FieldInsnNode insn = (FieldInsnNode) abstractInsn;
+        final FieldInsnNode fieldInsnNode =
+            findMatchingInsn(method.instructions, FieldInsnNode.class, (insnNode, index) ->
+                insnNode.getOpcode() == GETSTATIC
+                    && insnNode.owner.equals(obfuscated ? "ahm"   : "net/minecraft/world/WorldType")
+                    && insnNode.name.equals(obfuscated  ? "e"     : "field_151360_e")
+                    && insnNode.desc.equals(obfuscated  ? "Lahm;" : "Lnet/minecraft/world/WorldType;"));
 
-            if (!insn.owner.equals(obfuscated ? "ahm" : "net/minecraft/world/WorldType")
-                    || !insn.name.equals(obfuscated ? "e" : "field_151360_e")
-                    || !insn.desc.equals(obfuscated ? "Lahm;" : "Lnet/minecraft/world/WorldType;"))
-                continue;
-            final LabelNode amplifiedHandlerLabel = new LabelNode();
+        final InsnList worldTypeBOPAmplifiedCheckInsnList = new InsnList();
+        worldTypeBOPAmplifiedCheckInsnList.add(
+            new MethodInsnNode(
+                INVOKESTATIC,
+                Type.getInternalName(getClass()),
+                "isAmplifiedHook",
+                "(Lnet/minecraft/world/WorldType;)Z",
+                false));
+        worldTypeBOPAmplifiedCheckInsnList.add(
+            new JumpInsnNode(
+                IFEQ,
+                ((JumpInsnNode) fieldInsnNode.getNext()).label));
 
-            final InsnList isAmplifiedInsn = new InsnList();
-            isAmplifiedInsn.add(
-                new MethodInsnNode(
-                    INVOKESTATIC,
-                        Type.getInternalName(getClass()),
-                        "isAmplified",
-                        "(Lnet/minecraft/world/WorldType;)Z",
-                        false));
-            isAmplifiedInsn.add(
-                new JumpInsnNode(IFNE, amplifiedHandlerLabel));
+        method.instructions.insertBefore(fieldInsnNode, worldTypeBOPAmplifiedCheckInsnList);
 
-            method.instructions.insertBefore(insn, isAmplifiedInsn);
-            method.instructions.insert(insn.getNext(), amplifiedHandlerLabel);
-
-            /* 272: getstatic     #97 */
-            method.instructions.remove(insn.getNext());
-            /* 275: if_acmpne     302 */
-            method.instructions.remove(insn);
-            return;
-        }
+        method.instructions.remove(fieldInsnNode.getNext());
+        method.instructions.remove(fieldInsnNode);
     }
 
-    public static boolean isAmplified(final WorldType worldType)
-    {
+    @FunctionalInterface
+    private interface InsnNodePredicate<T extends AbstractInsnNode> {
+        boolean test(T insnNode, int index);
+    }
+
+    @SuppressWarnings({ "SameParameterValue", "unchecked" })
+    private static <T extends AbstractInsnNode> T findMatchingInsn(final InsnList insnList,
+                                                                   final Class<T> clazz,
+                                                                   final InsnNodePredicate<T> predicate) {
+        final ListIterator<AbstractInsnNode> iterator = insnList.iterator();
+
+        int i = 0;
+        while (iterator.hasNext()) {
+            final AbstractInsnNode insnNode = iterator.next();
+            if (clazz.isInstance(insnNode) && predicate.test((T) insnNode, i++))
+                return (T) insnNode;
+        }
+        throw new NoSuchElementException();
+    }
+
+    @SuppressWarnings("unused")
+    public static boolean isAmplifiedHook(final WorldType worldType) {
         return (worldType == WorldType.AMPLIFIED) || (worldType == BOPBiomes.worldTypeBOPAmplified);
     }
 }
